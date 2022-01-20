618f81351ff8284148f4f8b4a90388a386551fc7
Haocheng Wu <antertant@outlook.com>
2022-01-20 14:21:29 -0500

    Solved react router problem

---
+++ easyblog-backend/src/main/java/com/h349wu/easyblog/config/ReactRouteConfiguration.java
@@ -0,0 +1,23 @@
+package com.h349wu.easyblog.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Configuration
+public class ReactRouteConfiguration implements ErrorViewResolver
+{
+
+    @Override
+    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+        if (status == HttpStatus.NOT_FOUND) {
+            return new ModelAndView("forward:/");
+        }
+        return null;
+    }
+
+}
